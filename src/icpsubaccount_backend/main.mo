ff8cddd9130e9c72135a4c2ceb17dac5876441cc
diogodcmdcm <diogo.micheluzzi@gmail.com>
2024-12-03 13:02:09 -0300

    Adicionado o projeto

--- src/icpsubaccount_backend/main.mo
+++ src/icpsubaccount_backend/main.mo
@@ -1,5 +1,82 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Principal "mo:base/Principal";
+import Array "mo:base/Array";
+import Blob "mo:base/Blob";
+import Text "mo:base/Text";
+import Nat8 "mo:base/Nat8";
+import Nat "mo:base/Nat";
+import Iter "mo:base/Iter";
+import Debug "mo:base/Debug";
+import HashMap "mo:base/HashMap";
+
+actor IcpSubAccount {
+
+  var accounts = HashMap.HashMap<Principal, Blob>(10, Principal.equal, Principal.hash);
+
+  // Função para retornar o principal do usuário que está autenticado
+  public query (message) func get_principal_client() : async Text {
+      return Principal.toText(message.caller);
   };
+
+  // Função para criar uma subconta baseada em um identificador único
+  public func create_subaccount(userId: Text) : async Blob {
+      // O identificador do usuário é transformado em um Blob de 32 bytes
+      let hash = Text.encodeUtf8(userId); 
+
+      let hashArray = Blob.toArray(hash); //converte em array para percorrer os itens
+
+      var subaccount : [var Nat8] = Array.init<Nat8>(32, 0); // cria array de 32 bytes zerados
+
+      for (i in Iter.range(0, Nat.min(31, hash.size() - 1))) {
+          subaccount[i] := hashArray[i];
+      };
+
+      let immutableSubaccount = Array.freeze(subaccount); // converte para array imutavel, necessário para executar a função Blob.fromArray
+      return Blob.fromArray(immutableSubaccount);
+
+  };
+
+  // Atribuir uma subconta a um usuário autenticado
+  public shared(msg) func assign_subaccount(userId: Text): async Blob {
+
+      var subaccount : Blob = Blob.fromArray([]);
+
+      if(not Principal.isAnonymous(msg.caller) ){
+
+        //Cria uma Subacount com base no identificador informado
+        subaccount := await create_subaccount(userId);
+        
+        /* Armazena a SubAccount para o principal. Esta abordagem deverá ser tratada caso a caso de acordo com o perfil do projeto,
+           dependendo da quantidade de SubAccounts que serão armazenadas outras abordagens podem ser melhores.
+        */
+        accounts.put(msg.caller, subaccount);
+
+        // o bloco de comandos abaixo poderá ser dispensado, ele foi criado apenas para exibição dos dados na tela.
+        let textOption : ?Text = Text.decodeUtf8(subaccount);
+        switch (textOption) {
+          case (null) { 
+            // O blob não contém uma sequência UTF-8 válida
+            Debug.print("O blob não pôde ser decodificado como UTF-8");
+          };
+          case (?text) {                     
+            Debug.print("Subconta criada: " # text);
+          };
+        };
+
+      };     
+      
+      return subaccount;
+  };
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
--- src/icpsubaccount_frontend/src/App.jsx
+++ src/icpsubaccount_frontend/src/App.jsx
@@ -1,31 +1,102 @@
 import { useState } from 'react';
-import { icpsubaccount_backend } from 'declarations/icpsubaccount_backend';
+import {createActor, icpsubaccount_backend} from 'declarations/icpsubaccount_backend';
+import {AuthClient} from "@dfinity/auth-client"
+import {HttpAgent} from "@dfinity/agent";
 
-function App() {
-  const [greeting, setGreeting] = useState('');
+let actorBackend = icpsubaccount_backend;
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    icpsubaccount_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+function App() {  
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Utilizado para apresentar os botão Login e Logout
+
+  async function gerarSubAccount() {
+
+      // Criar o authClient
+      let authClient = await AuthClient.create();
+
+      // Inicia o processo de login e aguarda até que ele termine
+      await authClient.login({
+        // Redireciona para o provedor de identidade da ICP (Internet Identity)
+        identityProvider: "https://identity.ic0.app/#authorize",
+        onSuccess: async () => {   
+          // Caso entrar neste bloco significa que a autenticação ocorreu com sucesso!
+          const identity = authClient.getIdentity();
+          console.log("Principal obtido no frontend: " + identity.getPrincipal().toText()); // Já é possivel ter acesso ao Principal do usuário atenticado         
+          
+          /* A identidade do usuário autenticado poderá ser utilizada para criar um HttpAgent.
+             Ele será posteriormente utilizado para criar o Actor (autenticado) correspondente ao Canister de Backend  */
+          const agent = new HttpAgent({identity});
+          /* O comando abaixo irá criar um Actor Actor (autenticado) correspondente ao Canister de Backend  
+            desta forma, todas as chamadas realizadas a metodos SHARED no Backend irão receber o "Principal" do usuário */
+            actorBackend = createActor(process.env.CANISTER_ID_ICPSUBACCOUNT_BACKEND, {
+              agent,
+          });          
+          
+          //Obtem o Principal autenticado no Actor
+          const principalText = await actorBackend.get_principal_client();          
+
+          let userId = document.getElementById("userId").value;
+          //Gera uma SubAccount para o usuário autenticado com base no identificador informado
+          const subAccountText = await actorBackend.assign_subaccount(userId);                    
+          
+          document.getElementById("principalText").innerText = principalText;        
+          document.getElementById("subAccountText").innerText = subAccountText;        
+
+          console.log(principalText);
+          console.log(subAccountText);
+
+          setIsLoggedIn(true);
+
+        },
+        
+        windowOpenerFeatures: `
+                                left=${window.screen.width / 2 - 525 / 2},
+                                top=${window.screen.height / 2 - 705 / 2},
+                                toolbar=0,location=0,menubar=0,width=525,height=705
+                              `,
+      })
+      
+      return false;
+      
+  };
+ 
+
+  document.addEventListener("DOMContentLoaded", function() {    
+     document.getElementById("logout").style.display = "none";
+  });
 
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+
+      <div class="instructions-container">
+        <h1>Para gerar uma SubAccount siga os passos abaixo:</h1>
+        <ul class="instructions-list">
+          <li>Informe um identificador para gerar a SubAccount.</li>
+          <li>Clique no botão <strong>Gerar SubAccount</strong>. Caso o usuário não estiver autenticado na rede, será solicitado o Internet Identity.</li>
+          <li>Será gerado um número para a SubAccount no formato de array de 32 bytes (Blob).</li>
+        </ul>       
+
+        <div class="container_fields">
+          <label>Informe um identificador único: </label>
+          <input type="text" id="userId"  />
+          <button id="gerar" onClick={gerarSubAccount}>Gerar SubAccount</button> 
+        </div>     
+
+      </div>
+
+      <br/>
+      
+      <div class="container">
+        <label>Principal: </label><label id="principalText"></label>
+        <br/>
+        <label>SubAccount: </label><label id="subAccountText"></label>
+      </div>               
+      
     </main>
   );
 }
 
 export default App;
+
